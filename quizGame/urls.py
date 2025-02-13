75d4e63e1478c849d4a381d51a302483d4738333
annup76779 <annup76779@gmail.com>
2025-02-14 00:48:07 +0530

    - First API created

--- quizGame/urls.py
+++ quizGame/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from quizGame.views import home_page
+from quizGame.views import home_page, SubjectAreaApis
 
 urlpatterns = [
     path('', home_page, name="index_page"), # default path
+    path('subjectarea', SubjectAreaApis.as_view(), name="get_subjectareas")
 ]
--- quizGame/views.py
+++ quizGame/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import SubjectArea
 
 
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html')
+
+
+# region APIs
+
+class SubjectAreaApis(APIView):
+    def get(self, request):
+        subjectAreas = SubjectArea.objects.all()  # select * from quizGame_subjectarea;
+        return Response([
+            {"id": data.id, "name": data.name}
+            for data in subjectAreas
+        ])
+
+# endregion
